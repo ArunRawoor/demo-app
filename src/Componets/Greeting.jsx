13ba26c9bcf36b8840d5f6d646a20f5a69fc9de2
ArunRawoor <arun12831@gmail.com>
2026-04-09 17:06:12 +0700

    my - first commit

---
+++ src/Componets/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+return (
+<div className='greeting'>
+<h2>Welcome to GrowTech!</h2>
+<p>Bengaluru's best Full Stack program.</p>
+</div>
+)
+}
+export default Greeting
---
+++ src/Componets/PrviosCount.jsx
@@ -0,0 +1,45 @@
+// import React,{useState} from 'react'
+
+// function PrviosCount() {
+//     const initialCount =5
+// const [count , setCount] = useState(initialCount);
+
+// // const reset =() =>{
+// //     setCount(initialCount)
+// // }
+// const increment = () =>{
+//       setCount(count + 1);
+// }
+
+// const decrement = () =>{
+//       setCount(count - 1);
+// }
+// // const IncrementFive = () =>{
+// //     setCount(count + 5);
+// // }
+//   return (
+//     <div>
+//        <h1>Count : {count}</h1>
+//        {/* <button onClick={reset}>Reset</button> */}
+//        <button onClick={increment}>Increment</button>
+//        <button onClick={decrement}>Decrement</button>
+//        {/* <button onClick={IncrementFive}>+5Increment</button> */}
+
+
+//     </div>
+//   )
+// }
+
+// export default PrviosCount
+
+
+
+import React from 'react'
+
+function PrviosCount() {
+  return (
+    <div>PrviosCount</div>
+  )
+}
+
+export default PrviosCount
